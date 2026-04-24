08e4b939aa347e080fbb0228842470813e2ace27
aeroyhomeofdesign-tech <aeroyhomeofdesign@gmail.com>
2026-04-24 21:33:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import App from './FarmerVoice.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
